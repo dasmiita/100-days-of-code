33ef3043c78c9b53b9f6ad8eb56645ce211623e7
dasmiita <185056260+dasmiita@users.noreply.github.com>
2025-02-11 20:01:10 +0530

    Add files via upload

---
+++ day01.java
@@ -0,0 +1,8 @@
+//this is my first day of 100 days of code challenge
+class day01{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+/* 1. Write a program that prints "Hello World!" to the console
+2. Add single line and multi-line comments to your code */
