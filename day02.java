2d77cd924e308f936a989b373215d3c7f0c71908
dasmiita <185056260+dasmiita@users.noreply.github.com>
2025-02-12 23:34:19 +0530

    Add files via upload

---
+++ day02.java
@@ -0,0 +1,13 @@
+/*1. Declare and initialize variables for various data types including integer (int), floating-point number (double), and string (String). Then, print each variable to the console.
+ */
+public class day02{
+    public static void main(String[] args) {
+        int num =23;
+        String name = "Smi";
+        double percent = 90.2;
+        System.out.println(num);
+        System.out.println(name);
+        System.out.println(percent);
+
+    }
+}
